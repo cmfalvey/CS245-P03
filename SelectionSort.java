ef01342ad0764712a25960b19d3879e93f7e787e
cmfalvey <cmfalvey@dons.usfca.edu>
2017-09-18 22:21:55 -0700

    Add files via upload
    @toropeza @ShayRomayo @dbrizan

--- SelectionSort.java
+++ SelectionSort.java
@@ -1,26 +1,47 @@
-
+/* File:	SelectioneSort.java
+ * Purpose:	Sort an integer array using Selection Sort
+ * 
+ * Input:	a:	Unsorted Integer Array
+ * Output:	a:	Sorted Integer Array
+ * 
+ * Notes:	O(n^2) worst time
+ */
 public class SelectionSort implements SortingAlgorithm{
 
 	@Override
-	public void sort(int[] a) {
-		int temp = 0;
-		int i = 0, j = 0, min = 0;
-		
-		for (i = 0; i < a.length - 1; i++) {
-			min = i;
-			for (j = i + 1; j < a.length; j++) {
-				if (a[j] < a[min]) {
-					min = j;
-				}
-			}
-		}
-		
-		if (min != i) {
-			temp = a[i];
-			a[i] = a[j];
-			a[j] = temp;
-		}
-		
-	}
+	/* Function:	sort
+	 * Purpose:		sort an array and call swap to sort with selection sort
+	 * Input args:	int[] a:	unsorted array
+	 * Output args:	int[] a:	sorted array
+	 */
+	public void sort(int[] a) {	 
+		int outer = 0;
+		while (outer < a.length - 1) {
+            int min = outer;
+            int inner = outer + 1;
+            while (inner < a.length) {
+                if (a[inner] < a[min]) {
+                	min = inner;
+                }
+                inner++;
+            }
+            
+            swap(a, min, outer);
+            outer++;
+        }
+	} /*	sort	*/
+	
+	/* Function:	swap
+	 * Purpose:		swap two positions in an array
+	 * Input args:	int[] a:		unsorted array
+	 * 				int indexOne:	position one to swap
+	 * 				int indexTwo:	position two to swap
+	 * Output args:	int[] a:		array with two swapped values
+	 */
+	public void swap(int[] a, int indexOne, int indexTwo) {
+		int temp = a[indexOne];
+		a[indexOne] = a[indexTwo];
+		a[indexTwo] = temp;
+	} /*	swap	*/
 
 }
